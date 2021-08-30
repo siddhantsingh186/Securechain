1e56cd008714da26a604f4a61e36d558e76b1c60
Siddhant <201951151@iiitvadodara.ac.in>
2021-08-30 22:59:11 +0530

    Added Route

--- src/App.js
+++ src/App.js
@@ -1,55 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Home from './components/home/Home';
+import Nav from './components/header/Nav';
+import About from "./components/about/About";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 import './App.scss';
-import GoogleLogin from 'react-google-login';
-//import {NavLink} from 'react-router-dom';
 
-function App(props) {
-  const responseGoogle = (response)=>{
-    console.log(response);
-  }
-  return (
-    <div className="App">
-      <div className="App__header">
-        <div className="App__header__company">
-          <p>Secure<span style={{fontWeight:`400`}}>chain</span></p>
-        </div>
-        <ul className="App__header__list">
-          <li className="App__header__list__element">Home</li>
-          <li className="App__header__list__element">About</li>
-          <li className="App__header__list__element">Product</li>
-        </ul>
-        <GoogleLogin className="App__header__button"
-        clientId="1032167174701-bscvulohd5d6lpppmga4341uguhvma86.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-        isSignedIn={true}
-        />
-      </div>
-      <div className="App__above">
-        <div className="App__above__image" style={{backgroundImage:`url(media/home.jpg)`}}>
-          <div className="App__above__image__text">
-            <div className="App__above__image__text__container">
-              <h1 className="App__above__image__text__container__head">Building Safe and Transparent Supply Chains</h1>
-              <p className="App__above__image__text__container__paragraph">A decentralized blockchain-based application to manage the flow of goods and services in a supply chain.</p>
-              <div className="App__above__image__text__container__button">
-                <button className="App__above__image__text__container__button__button1"type="button">Learn More</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="App__below">
-        <ul className="App__below__list">
-          <li className="App__below__list__item">Decentralized Access</li>
-          <li className="App__below__list__item">Monitoring feature</li>
-          <li className="App__below__list__item">Security</li>
-          <li className="App__below__list__item">Customizable templates</li>
-        </ul>
-      </div>
+class App extends Component {
+  render() {
+    return (
+      <div className = "app">
+      <Router>
+        <Route path="/" component={Nav}></Route>
+        <Route path="/home" component={Home}></Route>
+        <Route path = "/about" component = {About}></Route>
+        <Redirect to ="/home" />
+      </Router>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
