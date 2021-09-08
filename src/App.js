19dbbde080503256f468e43299418074f382274e
Anuj Pillai <56990215+anuj-2001@users.noreply.github.com>
2021-09-08 16:38:59 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, {Component} from 'react';
 import Home from './components/home/Home';
 import Nav from './components/header/Nav';
 import About from "./components/about/About";
-import Error from "./components/error/Error";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Register from "./components/registration/register";
+import Login from "./components/registration/login";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Redirect} from 'react-router-dom';
 import './App.scss';
 
@@ -12,18 +13,12 @@ class App extends Component {
     return (
       <div className = "app">
       <Router>
-        <Nav />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route path = "*">
-            <Error />
-          </Route>
-        </Switch>
+        <Route path="/" component={Nav}></Route>
+        <Route exact path="/home" component={Home}></Route>
+        <Route exact path = "/about" component = {About}></Route>
+        <Route exact path = "/register" component = {Register}></Route>
+        <Route exact path = "/login" component = {Login}></Route>
+        {/* <Redirect to ="/home" /> */}
       </Router>
     </div>
     )
