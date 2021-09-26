80f4f27d79f3125b8a8b26a1a2db9a21cdff6555
Anuj Pillai <56990215+anuj-2001@users.noreply.github.com>
2021-09-26 21:34:06 +0530

    Update : connected backend

--- src/components/registration/login.js
+++ src/components/registration/login.js
@@ -6,7 +6,7 @@ const login = () => {
     return (
         <div>
             <div className="login_background_image" style={{backgroundImage:`url(media/back2.jpg)`}}>
-                <div className="container">
+                <div className="container-1">
                     <form>
                         <div className="Title">
                             <h1>Login</h1>
@@ -19,11 +19,11 @@ const login = () => {
                         </div>
                         <div className="form-group">
                             <label className="formLabel" htmlFor="email">Email</label><br/>
-                            <input className="formInput" type="text" name="email" id="email"></input>
+                            <input className="formInput" type="text" name="email" id="email" required></input>
                         </div>
                         <div className="form-group">
                             <label className="formLabel" htmlFor="password">Password</label><br/>
-                            <input className="formInput" type="password" name="password" id="password"></input>
+                            <input className="formInput" type="password" name="password" id="password" required></input>
                         </div>
                         <div className="form-submit">
                             <input className="formButton" type="submit" value="Submit"></input>
