cf8289312029374e120aae55478c51bf5cdcd281
Siddhant <201951151@iiitvadodara.ac.in>
2021-09-29 12:55:05 +0530

    fix:Established flow of owner

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import "./Dashboard.scss";
+import { useHistory } from 'react-router';
 
 function Dashboard(){
+  let history = useHistory();
+  const handleClick = (event) => {
+    history.push('/createsupplyhome');
+  }
   return(
     <div className = "dashboard">
       <h1 className = "dashboard__head">Dashboard</h1>
@@ -16,6 +21,9 @@ function Dashboard(){
           <p className = "dashboard__below__element__body">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <br />when an unknown printer took a galley of type and scrambled it to make a type specimen book.
           </p>
         </div>
+      </div>
+      <div className = "dashboard__button">
+        <button className = "dashboard__button__style" onClick = {handleClick}>Create New Supply Chain</button>
       </div>
     </div>
   );
