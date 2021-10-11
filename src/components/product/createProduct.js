700e2ccf2e6d052236b8309405764944412c02fb
Akshat Dobriyal <akcooldobriyal@gmail.com>
2021-10-11 20:04:48 +0530

    Updated createProduct.js

--- src/components/product/createProduct.js
+++ src/components/product/createProduct.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import './createProduct.scss'
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
-import Axios from 'axios'
+import Axios from 'axios';
 
 const CreateProduct = () => {
+
+    let token = localStorage.getItem("token");
+
+    const [supplyChain, setSupplyChain] = useState([]);
+    const [productSupplyChain, setProductSupplyChain] = useState({});
+    const [productName, setProductName] = useState({});
+    const [productUnits, setProductUnits] = useState({});
+
+    useEffect(() => {
+        axios
+            .get('https://securechain-backend.herokuapp.com/supplychain/',
+                {
+                    headers: {
+                        Authorization: `Token ${token}`
+                    }
+                }
+            )
+            .then((res) => {
+                if (res) {
+                    setSupplyChain(res.data);
+                    console.log(res.data)
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
     return(
         <div className="createsupply__bottom">
             <h1 className = "createsupply__bottom__head">Create Product</h1>
@@ -12,21 +40,41 @@ const CreateProduct = () => {
                     <form>
                         <div className="form-group">
                             <label>Select Supply Chain : </label>
-                            <select>
-                                <option>Vaccine Supply Chain</option>
-                                <option>Milk Supply Chain</option>
-                                <option>Car Supply Chain</option>
-                                <option>Bottle Supply Chain</option>
+                            <select
+                                name="supplyChains"
+                                id="supplyChains"
+                                onChange={(e) => { setProductSupplyChain(e.target.value) }}
+                            >
+                                <option value="">
+                                    Choose
+                                </option>
+                                {supplyChain.map((supplychain) => {
+                                    return (
+                                        <option key={supplychain.id} value={supplychain.id}>
+                                            {supplychain.name}
+                                        </option>
+                                    );
+                                })}
                             </select>
                         </div>
                         <h1> Product Details</h1>
                         <div className="form-group">
                             <label>Product Name : </label>
-                            <input name="name" type="text" id="name"></input>
+                            <input 
+                                name="name"
+                                type="text"
+                                id="name"
+                                onChange={(e) => {setProductName(e.target.value)}}
+                            />
                         </div>
                         <div className="form-group">
                             <label>Units Manufactured : </label>
-                            <input name="unit" type="text" id="unit"></input>
+                            <input
+                                name="unit"
+                                type="text"
+                                id="unit"
+                                onChange={(e) => { setProductUnits(e.target.value) }}
+                            />
                         </div>
                         
                         <button className="formButton" type="submit" >Add Product</button>
