6e77dc23e392cd7e0a40129f1249e15e067c6e0d
Siddhant <201951151@iiitvadodara.ac.in>
2021-11-06 12:27:19 +0530

    feat: Added hamburger

--- client/src/components/progress/progress.js
+++ client/src/components/progress/progress.js
@@ -3,85 +3,12 @@ import "./progress.scss"
 function Progress () {
     return (
     <div className = "progress">
-      <h1 className = "progress__head">Progress</h1>
-        <div class="p-4">
-            <h3 className="ordertracking">Order Tracking</h3>
-            <table class="table table-bordered track_tbl">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>S No</th>
-                        <th>Status</th>
-                        <th>Distibutor</th>
-                        <th>Date/Time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="active">
-                        <td class="track_dot">
-                            <span class="track_line"></span>
-                        </td>
-                        <td>01</td>
-                        <td>Dispatched from distibutor address</td>
-                        <td>Flipkart</td>
-                        <td>31/07/2018 22:24:PM</td>
-                    </tr>
-                    <tr>
-                        <td class="track_dot">
-                            <span class="track_line"></span>
-                        </td>
-                        <td>01</td>
-                        <td>Dispatched from distibutor address</td>
-                        <td>Flipkart</td>
-                        <td>31/07/2018 22:24:PM</td>
-                    </tr>
-                    <tr>
-                        <td class="track_dot">
-                            <span class="track_line"></span>
-                        </td>
-                        <td>01</td>
-                        <td>Dispatched from distibutor address</td>
-                        <td>Flipkart</td>
-                        <td>31/07/2018 22:24:PM</td>
-                    </tr>
-                    <tr>
-                        <td class="track_dot">
-                            <span class="track_line"></span>
-                        </td>
-                        <td>01</td>
-                        <td>Dispatched from distibutor address</td>
-                        <td>Flipkart</td>
-                        <td>31/07/2018 22:24:PM</td>
-                    </tr>
-                    <tr>
-                        <td class="track_dot">
-                            <span class="track_line"></span>
-                        </td>
-                        <td>01</td>
-                        <td>Dispatched from distibutor address</td>
-                        <td>Flipkart</td>
-                        <td>31/07/2018 22:24:PM</td>
-                    </tr>
-                    <tr>
-                        <td class="track_dot">
-                            <span class="track_line"></span>
-                        </td>
-                        <td>01</td>
-                        <td>Dispatched from distibutor address</td>
-                        <td>Flipkart</td>
-                        <td>31/07/2018 22:24:PM</td>
-                    </tr>
-                    <tr>
-                        <td class="track_dot">
-                            <span class="track_line"></span>
-                        </td>
-                        <td>01</td>
-                        <td>Dispatched from distibutor address</td>
-                        <td>Flipkart</td>
-                        <td>31/07/2018 22:24:PM</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className ="progress__head">
+            <p>Progress</p>
+        </div>
+        <div className = "progress__bottom">
+            <div className = "progress__left">Time</div>
+            <div className = "progress__right">Status</div>
         </div>
     </div>
     )
